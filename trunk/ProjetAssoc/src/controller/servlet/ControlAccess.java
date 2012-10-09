a554414900fa2b1de6e9d56dc71f7f25ea9d018b
zakaria.oualkadi@gmail.com <zakaria.oualkadi@gmail.com@dbee883b-f6da-2eff-3f32-90757e9ffd7f>
2012-10-09 08:39:10 +0000

    Debut de jsp login ;)

---
+++ trunk/ProjetAssoc/src/controller/servlet/ControlAccess.java
@@ -0,0 +1,59 @@
+package controller.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ControlAccess
+ */
+public class ControlAccess extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	HttpSession session;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControlAccess() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	
+	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.setContentType("text/html");
+		session = request.getSession();
+		String user = request.getParameter("user");
+		String password = request.getParameter("password");
+		session.setAttribute("user", user);
+		session.setAttribute("password", password);
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello world ! </h1>");
+		out.println("<h1>getServletName() : " +
+				getServletName() + "</h1>");
+		out.println("<h1>URL : " +
+				request.getRequestURI() + "</h1>");
+		out.println("<h1>USER : "+user+"</h1>");
+		out.println("<h1>PASS : "+password+"</h1>");
+		
+	}
+
+}
