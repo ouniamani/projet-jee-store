9a03853d293f5bf4d2cead1b47a2312dcd561cc0
zakaria.oualkadi <zakaria.oualkadi@gmail.com>
2012-10-10 13:24:36 +0000

    Modification ClientService constructeur. Ajour de la classe ArticleService

--- ProjetAssoc/src/controller/service/ClientService.java
+++ ProjetAssoc/src/controller/service/ClientService.java
@@ -8,20 +8,14 @@ import model.Client;
 
 public class ClientService {
 
-	private EntityManagerFactory emf;
-	public ClientService() {
-		emf = Persistence.createEntityManagerFactory("ProjetAssoc");
+	private EntityManager em;
+	public ClientService(EntityManager pem) {
+		em = pem;
 	}
 	
 	public boolean checkUserPassword(String user, String password){
-		//Creation d'un manager
-		EntityManager em = emf.createEntityManager();
-		
 		//Controle de l'utilisateur
 		Client my_cli = em.find(Client.class, user);
-		
-		//Fermeture du manager
-		em.close();
 		
 		return (my_cli != null && my_cli.getMdp().equals(password) ? true:false);
 		
